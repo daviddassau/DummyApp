96547d2738f8216a7dfb5de4969af5fcf88af3d6
David Dassau <davidtdassau@gmail.com>
2020-03-13 16:16:39 -0500

    Initial commit

---
+++ DummyApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DummyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
